bea501f84bce14cec032edd750c36499ba538bf3
ummyers <mu.andrea@ciencias.unam.mx>
2021-05-18 18:47:09 -0500

    Metodos en scala

--- FMetodos2.scala
+++ FMetodos2.scala
@@ -1,20 +1,19 @@
 /* Se implementa la funcion del ejercicio 1 */
 
-def time(speed: Double): Double = 
+def time(speed: Double): Double = {
+    // 
+    val distance = 42.195
+    val duration = distance / speed
+    duration * 60
+}
+
+def timeName(speed: Double, nombre: String): String = { 
     val distance = 42.19
     val duration = distance / speed
-    val minutes = duration * 60
+    nombre + " " + duration * 60
+}
+time(12.2)
+time(14.5)
 
-time(12)
-
-time(14)
-
-def timeName(speed: Double, nombre: String): Double = 
-    val distance = 42.19
-    val duration = distance / speed
-    val minutes = duration * 60
-    // val impresion = nombre + " corrió en tantos minutos " + minutes
-
-
-timeName(12,"Alice")
-timeName(14, "Bob")
+timeName(12.6,"Alice")
+timeName(14.6, "Bob")
